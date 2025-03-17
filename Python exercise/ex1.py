a80eac8a1caf13f154294540305b13d483b78ee1
Jerry Huang <17321421572@qq.com>
2025-03-17 10:47:08 +0800

    Add athe python exercise in git bash

---
+++ Python exercise/ex1.py
@@ -0,0 +1,10 @@
+# Enter a Number and output twice its value in Python 
+# Author Jerry Huang
+# Using user input
+
+# taking user input
+a = input("Enter first numer:")
+
+res =int(a) * 2
+
+print(res)
---
+++ Python exercise/ex2.py
@@ -0,0 +1,5 @@
+# Prompt the user for their name
+name = input("Please enter your name: ")
+
+# Convert the name to uppercase and print it
+print("Your name in uppercase is:", name.upper())
